575d783d7c377c028388a492b74255947c66db9c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-08-12 20:50:18 +0530

    Finished Contact tab

--- src/tabs/contact-tab.js
+++ src/tabs/contact-tab.js
@@ -1,5 +1,20 @@
 export const contact_tab = () => {
   event.target.classList.add('active')
   const main = document.querySelector('#main')
-  main.textContent = 'Contact'
+  const contact = document.createElement('div')
+  contact.id = 'contact-div'
+  const tempText = `
+    <div>
+    <strong>Clumsyknight's Restaurant</strong>
+    </div>
+    <div>
+    Location: Mumbai, Maharashtra, India 
+    </div>
+    <div>
+    Contact: You it this isn't real, right? 
+    </div>
+  `
+
+  contact.innerHTML = tempText
+  main.appendChild(contact)
 }
