e2c5bd53dee4fce0b3788382f9f96bdda2fd54e5
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-08-12 14:27:53 +0530

    Added navbar
    Need to find a way to add content in the navbar

--- src/heading.js
+++ src/heading.js
@@ -1,7 +1,7 @@
 export const Heading = () => {
     const content = document.querySelector('#content')
     const heading = document.createElement('div')
-    heading.id = 'bones'
+    heading.id = 'heading'
     heading.textContent = "Clumsy's Restaurant"
     content.appendChild(heading)
   }  
--- src/tabs.js
+++ src/tabs.js
@@ -1,4 +1,32 @@
 export const tabs = () => {
   const content = document.querySelector('#content')
-  console.log('tabs')
+  
+  const tabDiv = document.createElement('div')
+  tabDiv.id = 'tab-div'
+
+  const home = document.createElement('div')
+  home.id = 'home-tab'
+  home.classList.add('tabs')
+  home.textContent = 'Home'
+  
+  const menu = document.createElement('div')
+  menu.id = 'menu-tab'
+  menu.classList.add('tabs')
+  menu.textContent = 'Menu'
+  
+  const contact = document.createElement('div')
+  contact.id = 'contact-tab'
+  contact.classList.add('tabs')
+  contact.textContent = 'Contact'
+  
+  const about = document.createElement('div')
+  about.id = 'about-tab'
+  about.classList.add('tabs')
+  about.textContent = 'About'
+  
+  tabDiv.appendChild(home)
+  tabDiv.appendChild(menu)
+  tabDiv.appendChild(contact)
+  tabDiv.appendChild(about)
+  content.appendChild(tabDiv)
 }
