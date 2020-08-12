5fdfe43f97b3f1be7d0b8f51107d7c36bde91312
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-08-12 20:50:03 +0530

    Finished about page

--- src/tabs/about-tab.js
+++ src/tabs/about-tab.js
@@ -1,5 +1,17 @@
 export const about_tab = () => {
   event.target.classList.add('active')
   const main = document.querySelector('#main')
-  main.textContent = 'About'
+  const about = document.createElement('div')
+
+  const tempText = `
+    Hi, I am <strong>Clumsyknight</strong>, the guy who developed this page
+    <br/><br/>
+    You can view my github profile <a href='https://github.com/Clumsynite' target='_blank' class='jump-link'>here</a>
+    <br/><br/>
+    Or, you can look at code <a href='https://github.com/Clumsynite/js-restaurant-page' target='_blank' class='jump-link'>here</a>
+  `
+
+  about.innerHTML = tempText
+
+  main.appendChild(about)
 }
